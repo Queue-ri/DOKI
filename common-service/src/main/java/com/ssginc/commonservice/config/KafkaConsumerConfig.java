120269f4da2ac770194c9f7a7863452302fc47dd
Queue-ri <qriosity.dev@gmail.com>
2025-09-06 16:48:28 +0900

    refactor: Extract and organize hardcoded endpoints (#139)

--- common-service/src/main/java/com/ssginc/commonservice/config/KafkaConsumerConfig.java
+++ common-service/src/main/java/com/ssginc/commonservice/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.ssginc.commonservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -17,10 +18,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${kafka.host}")
+    private String host;
+
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.47.118.187:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "doki");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
